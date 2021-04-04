f8f4439d51889e988812caafabcb24a5282267c4
Ved Mahadeo <vedkailash1@gmail.com>
2021-04-03 21:05:17 -0400

    Add files via upload
    Added spotify authentication, the full top 10s functionality, and parts of the other options. To test any of the spotify features you have to use the live-server extension in vscode and make sure its ip is 127.0.0.1:5500

---
+++ js/app.js
@@ -0,0 +1,22 @@
+const client_id = "9a0a70b3ee894325b5e22053070e25c9";
+const client_secret = "5f2e8eb22cc64b39b248a92162cd3dc5";
+const redirect_uri = "http://127.0.0.1:5500/html/menu.html";
+const scopes = "user-read-private user-library-read user-top-read playlist-modify-private playlist-modify-public";
+
+localStorage.setItem("client_id", client_id);
+localStorage.setItem("client_secret", client_secret);
+localStorage.setItem("redirect_uri", redirect_uri);
+
+const AUTHORIZATION_URL = "https://accounts.spotify.com/authorize";
+
+
+function authUser () {
+    let url = AUTHORIZATION_URL + "?";
+    url += `client_id=${client_id}`;
+    url += `&response_type=code`;
+    url += `&redirect_uri=${redirect_uri}`;
+    url += `&scope=${scopes}`;
+    url += `&show_dialog=true`;
+
+    window.location.href = url;
+}
