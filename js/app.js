b72dca5a354e6f98260352eb993fc4ebf1234751
Ved Mahadeo <vedkailash1@gmail.com>
2021-04-11 12:03:32 -0400

    commit

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const client_id = "9a0a70b3ee894325b5e22053070e25c9";
 const client_secret = "5f2e8eb22cc64b39b248a92162cd3dc5";
 //const redirect_uri = "http://127.0.0.1:5500/html/menu.html";
-const redirect_uri = "https://spotstats-c95dd.web.app/menu.html";
+const redirect_uri = "https://spotstats-c95dd.firebaseapp.com/html/menu.html";
 const scopes = "user-read-private user-library-read user-top-read playlist-modify-private playlist-modify-public";
 
 localStorage.setItem("client_id", client_id);
